eed30be3c7087d3552875f8460fdf875652df406
Derek Maggio <derek.maggio@opentrons.com>
2022-03-31 19:14:43 -0700

    Edit main
    Remove generate_message
    Refactor to use new StrymonBigSky CLass

--- main.py
+++ main.py
@@ -4,25 +4,14 @@ import mido
 from hardware_definitions.big_sky import StrymonBigSky, FootswitchState, ReverbTypes
 
 
-def generate_message(channel: int, control_change: StrymonBigSky, val: int) -> mido.Message:
-    if val not in control_change.acceptable_vals:
-        print(val)
-        print(control_change.acceptable_vals)
-        raise ValueError("You passed an unacceptable value")
-
-    return mido.Message(
-        "control_change",
-        channel=channel,
-        control=control_change.cc_number,
-        value=val
-    )
-
-
 if __name__ == "__main__":
     usb_midi = mido.open_output('UM-ONE:UM-ONE MIDI 1 20:0')
-
-    message = StrymonBigSky.footswitch_a(0)
+    big_sky = StrymonBigSky(1)
+    message = big_sky.toggle_footswitch_a()
     usb_midi.send(message)
     sleep(1)
-    message = StrymonBigSky.footswitch_b(0)
+    message = big_sky.toggle_footswitch_b()
+    usb_midi.send(message)
+    sleep(1)
+    message = big_sky.toggle_footswitch_c()
     usb_midi.send(message)
