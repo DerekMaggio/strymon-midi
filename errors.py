69a136d4221b4731bf351242cf1581c8194c6321
Derek Maggio <derek.maggio@opentrons.com>
2022-03-31 19:14:48 -0700

    Add errors

---
+++ errors.py
@@ -0,0 +1,26 @@
+from typing import (
+    List,
+    Union,
+)
+
+
+class InvalidControlChangeValueError(Exception):
+    def __init__(self, value: int, acceptable_values: Union[range, List[int]]) -> None:
+
+        if isinstance(acceptable_values, range):
+            acceptable_values_str = f"{min(acceptable_values)}-{max(acceptable_values)}"
+        else:
+            acceptable_values_str = ', '.join(str(int(val)) for val in acceptable_values)
+
+        super().__init__(
+            f"Passed value {value} is not valid. \nAcceptable values "
+            f"are: {acceptable_values_str}"
+        )
+
+
+class InvalidChannelError(Exception):
+    def __init__(self, passed_channel: str) -> None:
+        super().__init__(
+            f"Passed value {passed_channel} is invalid."
+            f"\nChannel must be between 1 and 16"
+        )
